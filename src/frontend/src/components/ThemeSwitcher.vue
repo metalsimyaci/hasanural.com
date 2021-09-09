49da164f7f066152ca342c108889f39ad0e4ff71
metalsimyaci <metalsimyaci@outlook.com>
2021-09-09 17:52:25 +0300

    vue pnpm initial setup with dark them

---
+++ src/frontend/src/components/ThemeSwitcher.vue
@@ -0,0 +1,40 @@
+<template>
+    <a href="#" @click.prevent="toggleTheme">
+        <i
+            v-if="dheme === 'light'"
+            data-feather="moon"
+            class="
+                text-liText-ternary-dark
+                hover:text-gray-400
+                dark:text-liText-ternary-light dark:hover:text-li
+                Border-primary-light
+                w-5
+            "
+        ></i>
+        <i
+            v-else
+            data-feather="sun"
+            class="text-gray-200 hover:text-gray-50 w-5"
+        ></i>
+    </a>
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from "vue-router";
+const router = useRouter();
+const props = defineProps({
+    dheme: {
+        type: Object as () => string,
+        required: true,
+    },
+});
+const emit = defineEmits(["themeChanged"]);
+const toggleTheme = () => {
+    console.log("theme", props.dheme);
+    let newTheme = props.dheme == "light" ? "dark" : "light";
+    console.log("newTheme", newTheme);
+    localStorage.setItem("theme", newTheme);
+    emit("themeChanged", newTheme);
+    router.push({ name: "Home" });
+};
+</script>
